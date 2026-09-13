682e440fa6013c70c7522eb809e7d0ea49bce3fc
Damerath <damerath@web.de>
2026-09-13 22:10:41 +0200

    Fix stimilus errors

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,18 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
 eagerLoadControllersFrom("controllers", application)
-import FlashController from "./flash_controller"
-application.register("flash", FlashController)
-import MenuController from "./menu_controller"
-application.register("menu", MenuController)
-import CampApplicationController from "./camp_application_controller"
-application.register("camp-application", CampApplicationController)
-import ApplicationModalController from "./application_modal_controller"
-application.register("application-modal", ApplicationModalController)
-import DatepickerController from "./datepicker_controller"
-application.register("datepicker", DatepickerController)
-import ConfirmModalController from "./confirm_modal_controller"
-application.register("confirm-modal", ConfirmModalController)
-import NotificationsDrawerController from "./notifications_drawer_controller"
-application.register("notifications-drawer", NotificationsDrawerController)
